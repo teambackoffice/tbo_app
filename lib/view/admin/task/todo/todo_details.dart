3d069a2c51d916f30389994bf02639def76dc825
Najath Thekkeyil <najath.thekkeyil13@gmail.com>
2025-08-13 11:45:21 +0530

    To Do Detail page UI

--- lib/view/admin/task/todo/todo_details.dart
+++ lib/view/admin/task/todo/todo_details.dart
@@ -1,13 +1,141 @@
 import 'package:flutter/material.dart';
 
-class AdminToDoDetails extends StatelessWidget {
-  const AdminToDoDetails({super.key});
+class AdminTodoDetails extends StatelessWidget {
+  const AdminTodoDetails({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('todo Details')),
-      body: Center(child: Text('This is the todo details page.')),
+      backgroundColor: const Color(0xFFF9F7F3),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Back button
+              CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.white,
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back_ios_new_outlined),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ),
+              const SizedBox(height: 50),
+              // Card
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Priority Chip
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 4,
+                      ),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF1C7690),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Text(
+                        "High",
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    // Title
+                    const Text(
+                      "Champion Car Wash App",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: const [
+                        SizedBox(width: 6),
+                        Text(
+                          "Assigned To",
+                          style: TextStyle(color: Colors.grey, fontSize: 14),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 4),
+                    const Text(
+                      "Jasir",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    // Due Date
+                    // Time
+                    Row(
+                      children: const [
+                        Icon(Icons.access_time, size: 18),
+                        SizedBox(width: 6),
+                        Text(
+                          "Time",
+                          style: TextStyle(color: Colors.grey, fontSize: 14),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 4),
+                    const Text(
+                      "18 Hours",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    // Due Date
+                    Row(
+                      children: const [
+                        Icon(Icons.calendar_today, size: 18),
+                        SizedBox(width: 6),
+                        Text(
+                          "Due Date",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 4),
+                    const Text(
+                      "15-07-25",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    // Description
+                    const Text(
+                      "The Champion Car Wash App is designed to streamline your vehicle cleaning experience.\n With just a few taps, you can book a wash, track service time, and receive timely updates.\n Our professional team ensures high-quality cleaning while you focus on your day. The app also helps you manage appointments, track past services, and enjoy exclusive offers.",
+                      style: TextStyle(fontSize: 16, height: 1.4),
+                    ),
+                    const SizedBox(height: 20),
+
+                    // Button
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
