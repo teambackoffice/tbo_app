48fd4df5e979c0b855cebf1131ad1259a543bbbc
Najath Thekkeyil <najath.thekkeyil13@gmail.com>
2025-08-14 11:17:09 +0530

    Admin To-Do to Open

--- lib/view/admin/task/task.dart
+++ lib/view/admin/task/task.dart
@@ -65,7 +65,7 @@ class _AdminTaskPageState extends State<AdminTaskPage>
                 indicatorSize: TabBarIndicatorSize.tab,
                 dividerColor: Colors.transparent,
                 tabs: [
-                  _buildTab(Icons.access_time, "To Do"),
+                  _buildTab(Icons.access_time, "Open"),
                   _buildTab(Icons.incomplete_circle, "In Progress"),
                   _buildTab(Icons.check_circle, "Completed"),
                 ],
