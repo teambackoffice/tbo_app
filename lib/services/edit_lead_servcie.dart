18b9db732fa9715f54b67d427492c6ef3c424cc6
Najath Thekkeyil <najath.thekkeyil13@gmail.com>
2025-10-10 17:30:56 +0530

    QUOTATION SENT -> CONVERTED

--- lib/services/edit_lead_servcie.dart
+++ lib/services/edit_lead_servcie.dart
@@ -11,15 +11,9 @@ class EditLeadService {
     required String status,
   }) async {
     try {
-      print("🟡 Sending Edit Lead Request...");
-      print("📍 URL: $_url");
-
       var headers = {'Content-Type': 'application/json'};
 
       var body = json.encode({"lead_id": leadId, "status": status});
-
-      print("📦 Request Headers: $headers");
-      print("📦 Request Body: $body");
 
       var request = http.Request('POST', Uri.parse(_url));
       request.body = body;
@@ -27,25 +21,17 @@ class EditLeadService {
 
       http.StreamedResponse response = await request.send();
 
-      print("🕒 Waiting for response...");
-
       if (response.statusCode == 200) {
         final resBody = await response.stream.bytesToString();
         final decoded = json.decode(resBody);
 
-        print("✅ Response Status: ${response.statusCode}");
-        print("✅ Response Body: $decoded");
-
         return decoded;
       } else {
         final errorBody = await response.stream.bytesToString();
-        print("❌ Failed with status: ${response.statusCode}");
-        print("❌ Reason: ${response.reasonPhrase}");
-        print("❌ Response Body: $errorBody");
+
         return null;
       }
     } catch (e) {
-      print("⚠️ Exception in editLead: $e");
       return null;
     }
   }
